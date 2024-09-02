5b8acb4172d81aab090051b38641fb66376fe9f0
Allendale Nato <natoallendale@gmail.com>
2024-09-02 09:41:29 +0800

    Add 8D movement
    - Declare top_speed variable
    - Declare velocity variable

--- Player/ship.gd
+++ Player/ship.gd
@@ -1,6 +1,13 @@
 extends Sprite2D
+@export_group("Ship Properties")
+@export_range(100, 1000, 1.0, "suffix:px/s") var top_speed: int = 100.0
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var direction: Vector2 = Vector2.ZERO
+	var velocity: Vector2 = Vector2.ZERO
+
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	velocity = direction * top_speed
+	rotation = velocity.angle()
+	position += velocity * delta
