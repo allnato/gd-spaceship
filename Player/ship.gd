8484c26def30bd85e6dc07786c8864d118bb8459
Allendale Nato <natoallendale@gmail.com>
2024-09-02 15:01:48 +0800

    Add joystick support
    - Change movement deadzone from 0.5 to 0.2
    - Add joystick vibration when boosting

--- Player/ship.gd
+++ Player/ship.gd
@@ -7,7 +7,7 @@ var velocity: Vector2 = Vector2.ZERO
 @export_group("Ship Properties")
 @export_range(100.0, 1000.0, 1.0, "suffix:px/s") var base_speed: float = 300.0
 @export_range(100.0, 1000.0, 1.0, "suffix:ps/s") var boost_speed: float = 700.0
-@export_range(1, 5, 1, "suffix:sec") var boost_time: int = 1
+@export_range(1, 5, 1, "suffix:sec") var boost_time: float = 1
 @export_range(1, 100, 1) var steering_factor: float = 10.0
 
 
@@ -20,6 +20,7 @@ func _process(delta: float) -> void:
 	var target_vector: Vector2 = Vector2.ZERO
 
 	# Get Directional Vectors
+
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	# Normalize Directional Vector
@@ -30,15 +31,16 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ship_boost"):
 		# Change top_speed to boost_speed
 		top_speed = boost_speed
+		Input.start_joy_vibration(0, 0.0, 0.5, boost_time)
 		boost_timer.start(boost_time)
 
 	# Set Target Vector (TV)
 	target_vector = direction * top_speed
 	# Set Steering Vector (SV): subtract current velocity from TV
 	steering_vector = target_vector - velocity
-	
+
 	# Update Curr Velocity: Add SV and velocity
-	# Shorten the sum by multilying it to the steer factor and delta 
+	# Shorten the sum by multilying it to the steer factor and delta
 	velocity += steering_vector * steering_factor * delta
 	# Update player position using velocity
 	position += velocity * delta
