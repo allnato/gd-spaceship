dd3ce1cafcb80ac47502e308ae73bbe767a9318d
Allendale Nato <natoallendale@gmail.com>
2024-09-02 10:46:46 +0800

    Normalize directional vector and Rotate Sprite

--- Player/ship.gd
+++ Player/ship.gd
@@ -6,8 +6,20 @@ func _process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	var velocity: Vector2 = Vector2.ZERO
 
+	# Get Directional Vectors
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+	# Normalize Directional Vector
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+
+	# Calculate velocity by combining direction and speed
 	velocity = direction * top_speed
-	rotation = velocity.angle()
+
+	# Update player position using velocity
 	position += velocity * delta
+
+	# Rotate the sprite based off the velocity vector
+	# Only rotate if moving
+	if direction != Vector2.ZERO:
+		rotation = velocity.angle()
