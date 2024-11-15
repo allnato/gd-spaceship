ec93d5b8d693773449ab963bd2705a917fa899c6
Allendale Nato <natoallendale@gmail.com>
2024-11-15 15:11:06 -0600

    Normalize diagonal movement

--- Player/ship.gd
+++ Player/ship.gd
@@ -9,6 +9,10 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	
+	
 	# Apply speed
 	var velocity: Vector2 = direction * speed
 	position += velocity * delta
