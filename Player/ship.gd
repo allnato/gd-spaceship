8749e27a254d9609108a19a60cf3832fb1a08940
Allendale Nato <natoallendale@gmail.com>
2024-11-15 14:37:16 -0600

    Add simple ship movement
    Add basic movement that I can recall

--- Player/ship.gd
+++ Player/ship.gd
@@ -1,6 +1,14 @@
-extends Sprite2D
+extends Area2D
 
+@export var speed: = 300.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# Fetch directional inputs
+	var direction: Vector2 = Vector2.ZERO
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	# Apply speed
+	var velocity: Vector2 = direction * speed
+	position += velocity * delta
