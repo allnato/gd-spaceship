4815b0b1f48d6ca41be3883ac0e65cdf1be06b52
Allendale Nato <natoallendale@gmail.com>
2024-11-15 21:09:29 -0600

    Add ship rotation
    Rotate ship based on direction

--- Player/ship.gd
+++ Player/ship.gd
@@ -9,9 +9,13 @@ func _process(delta: float) -> void:
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	
+	# Normalize diagonal movements
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 	
+	# Rotate ship with regards to direction
+	if direction != Vector2.ZERO:
+		rotation = direction.angle()
 	
 	# Apply speed
 	var velocity: Vector2 = direction * speed
