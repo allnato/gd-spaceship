50b8ef2024d9949197b9d94e1a88ac1794221d97
Allendale Nato <natoallendale@gmail.com>
2024-11-16 15:35:49 -0600

    Add vector steering

--- Player/ship.gd
+++ Player/ship.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 @export var ship_speed: = 500.0
 @export var ship_boost_speed: = 800.0
+@export var ship_steer_factor: = 0.8
 @export var boost_time: = 1.5
-var speed:= 0.0
 @onready var BoostTimer: Timer = $ShipBoostTimer
 
+var current_speed:= 0.0
+var steering_velocity: Vector2 = Vector2.ZERO
+var target_velocity:Vector2 = Vector2.ZERO
+var current_velocity:= Vector2.ZERO
+
 func _ready() -> void:
-	speed = ship_speed
+	current_speed = ship_speed
 	BoostTimer.wait_time = boost_time
 	BoostTimer.one_shot = true
 
@@ -26,16 +31,18 @@ func _process(delta: float) -> void:
 	if direction != Vector2.ZERO:
 		rotation = direction.angle()
 
-	var velocity:= Vector2.ZERO
+
 	if Input.is_action_just_pressed("boost_ship"):
 		BoostTimer.start()
-		speed = ship_boost_speed
+		current_speed = ship_boost_speed
 
-	velocity = direction * speed
+	target_velocity = direction * current_speed
+	steering_velocity = (target_velocity - current_velocity) * delta * ship_steer_factor
+	current_velocity += steering_velocity
 
 	# Apply normal speed
-	position += velocity * delta
+	position += current_velocity * delta
 
 
 func _on_ship_boost_timer_timeout() -> void:
-	speed = ship_speed
+	current_speed = ship_speed
